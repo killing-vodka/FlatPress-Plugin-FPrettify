098ea4c46bf02a6bd0b36d4f4bdf70b85290ae97
Giovanni <pozharlab@gmail.com>
2016-04-04 09:44:48 +0200

    Added files via upload

---
+++ fprettify/plugin.fprettify.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Plugin Name: FPrettify
+Version: 0.1
+Plugin URI: https://github.com/pozharlab/FlatPress-Plugin-FPrettify
+Description: A script that makes source-code snippets in HTML prettier.
+Author: Giovanni Forte
+Author URI: http://www.pozharlab.net
+*/
+
+add_action('wp_head', 'plugin_fprettify_head', 0);
+
+function plugin_fprettify_head() {
+	$pdir=plugin_geturl('jquery');
+	echo <<<JSUTILS
+	<script src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js"></script>
+	<link rel="stylesheet" href="https://cdn.rawgit.com/google/code-prettify/master/src/prettify.css">
+JSUTILS;
+}
+
+function plugin_fprettyprint($string) {
+	$string = str_replace('<pre>', "<pre class=\"prettyprint\">", $string);
+	return $string;
+}						
+			
+add_filter('the_content', 'plugin_fprettyprint', 1);
+add_filter('comment_text', 'plugin_fprettyprint', 1);
+?>
